80dbe1521bbc7bab0b8e725e273e840ecdeec87c
TakingTheL <yliu6@stuy.edu>
2017-11-03 07:09:10 -0400

    laziness resolved

--- c.c
+++ c.c
@@ -8,37 +8,58 @@
 // Takes a slow and inefficient approach because I was too lazy to fix it
 // May be better in the future
 void printDirInfo(char *dirloc) {
-  printf("\nStatistics for diretory: %s\n", dirloc);
-  DIR *dir = opendir(dirloc);
 
   //Print size
-  int sum = 0;
-  struct dirent *curr;
+
+  
+  /*
   while (curr = readdir(dir)){ // Yes it does the same loop thrice
     struct stat ap;
     stat((*curr).d_name, &ap);
     if ((*curr).d_type == 8)
       sum += ap.st_size;
   }
+  */
 
-  //Print dirs
+ 
+  
+  struct dirent *curr;
+  DIR *dir = opendir(dirloc);
+  char *files[500];
+  char *directories[500];
+  int f = 0;
+  int d = 0;
+  int sum = 0;
+  while (curr = readdir(dir)){
+    struct stat ap;
+    if ((*curr).d_type == 4){
+      directories[d] = (*curr).d_name;
+      d ++;
+    }
+    else if ((*curr).d_type == 8){
+      files[f] = (*curr).d_name;
+      f ++;
+      sum += ap.st_size;
+    }
+  }
+
+  closedir(dir);
+
+  printf("\nStatistics for directory: %s\n", dirloc);
   printf("Total Directory Size: %d Bytes\n", sum);
-  printf("Directories:\n");
-  closedir(dir);
-  dir = opendir(dirloc);
-  while (curr = readdir(dir))
-    if ((*curr).d_type == 4)
-      printf("   %s\n", (*curr).d_name);
-
-  //Print files
-  printf("Regular Files:\n", sum);
-  closedir(dir);
-  dir = opendir(dirloc);
-  while (curr = readdir(dir))
-    if ((*curr).d_type == 8)
-      printf("   %s\n", (*curr).d_name);
-
-  closedir(dir);
+  int i = 0;
+  int a = 0;
+  printf("Subdirectories:\n");
+  while(directories[a]){
+    printf("    %s\n",directories[a]);
+    a ++;
+  }
+    printf("Regular Files:\n");
+  while (files[i]){
+    printf("    %s\n",files[i]);
+    i ++;
+  }
+  
 }
 
 int main() {
